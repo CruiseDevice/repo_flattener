f0ee536c34f632edf171ed7da17248e271b2928b
CruiseDevice <achavan1211@gmail.com>
2025-03-13 23:58:10 -0700

    add files to git

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Setup script for repo-flattener
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-flattener",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'repo-flattener=repo_flattener.cli:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
